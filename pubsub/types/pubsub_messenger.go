c3d3344fef36268ea4555be487311c0bc38444ad
YoungAgency <44611269+YoungAgency@users.noreply.github.com>
2018-11-30 15:26:30 +0100

    Added comments on PubSubMessenger interface

--- pubsub/types/pubsub_messenger.go
+++ pubsub/types/pubsub_messenger.go
@@ -3,7 +3,9 @@ package types
 import "context"
 
 // Defines our interface for connecting, producing and consuming messages.
+// Publish send the given obj message to given PubSub topicName
+// Subscribe perform a subscription to given topic. handlerFunc is invoked when a message is received
 type PubSubMessenger interface {
 	Publish(obj interface{}, topicName string) error
-	Subscribe(topic string, handlerFunc func(context.Context, string, int64, []byte) error) error
+	Subscribe(topic string, handlerFunc func(ctx context.Context, eventID string, timestamp int64, message []byte) error) error
 }
